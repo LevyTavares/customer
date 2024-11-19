ad7eeab17be5564cc031834d517ba189f7234147
LevyTavares <isaiaslevi2@gmail.com>
2024-11-19 14:51:13 -0300

    oi

--- desafio/js/desafio.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('queueForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var name = document.getElementById('nameInput').value;
-    var queueList = document.getElementById('queueItems');
-    var newItem = document.createElement('li');
-    newItem.textContent = name;
-    queueList.appendChild(newItem);
-    document.getElementById('nameInput').value = '';
-});
